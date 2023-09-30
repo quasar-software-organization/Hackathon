13b5f3c9ec3e794599155015ec95725f1fd8697b
ale24dev <ale.24dev@gmail.com>
2023-09-30 01:26:22 -0400

    project screen implemented

--- aidcode/lib/resources/app_assets.dart
+++ aidcode/lib/resources/app_assets.dart
@@ -4,6 +4,9 @@ class AppAssets {
   AppAssets._();
 
   static const String aidCodeWelcome = 'assets/images/aid_code_welcome.png';
+  static const String imageUserProfileMock =
+      'assets/images/image_user_profile_mock.png';
   static const String logoAidcodePng = 'assets/images/logo_aidcode_png.png';
+  static const String projectLogo = 'assets/images/project_logo.png';
   static const String logoAidcodeSvg = 'assets/svg/logo_aidcode_svg.svg';
 }
