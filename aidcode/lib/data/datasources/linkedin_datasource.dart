ae46d0f9b4b822ea1bae15bbffcf49df3aee7b05
daniarmas <daniel.armas9706@gmail.com>
2023-09-30 16:07:13 -0400

    Implementing the createPost method.

--- aidcode/lib/data/datasources/linkedin_datasource.dart
+++ aidcode/lib/data/datasources/linkedin_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 abstract class LinkedinDatasource {
@@ -9,12 +10,40 @@ abstract class LinkedinDatasource {
 
 @Injectable(as: LinkedinDatasource)
 class LinkedinDatasourceImpl implements LinkedinDatasource {
+  final Dio dio;
+
+  LinkedinDatasourceImpl({required this.dio});
+
   @override
   Future<void> createPost(
       {required String title,
       required String description,
-      required String nonProfitName}) {
-    // TODO: implement createPost
-    throw UnimplementedError();
+      required String nonProfitName}) async {
+    try {
+      await dio.post('/v2/ugcPosts', data: {
+        "author": "urn:li:person:98947521",
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+          "com.linkedin.ugc.ShareContent": {
+            "shareCommentary": {"text": title},
+            "shareMediaCategory": "ARTICLE",
+            "media": [
+              {
+                "status": "READY",
+                "description": {
+                  "text":
+                      description
+                },
+                "originalUrl": "https://blog.linkedin.com/",
+                "title": {"text": "Official LinkedIn Blog"}
+              }
+            ]
+          }
+        },
+        "visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"}
+      });
+    } catch (e) {
+      throw Exception('Error posting in Linkedin');
+    }
   }
 }
