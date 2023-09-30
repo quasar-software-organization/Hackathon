e1bc869defa6a3f2c8a75ebff92fc003a810096a
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-09-30 00:21:08 -0400

    empezando

--- aidcode/lib/presentation/screens/volunter/volunteer.dart
+++ aidcode/lib/presentation/screens/volunter/volunteer.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class VolunteerScreen extends StatelessWidget {
   const VolunteerScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+      body: Text("Volunter"),
     );
   }
 }
