e372f48316beb6fb10bba58a00f1c478a435590a
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-09-30 02:13:47 -0400

    more details on sliverappbar , part1 of project detail

--- aidcode/lib/presentation/widgets/sliver_app_bar.dart
+++ aidcode/lib/presentation/widgets/sliver_app_bar.dart
@@ -7,38 +7,54 @@ sliverAppBar(
   Widget? training,
   Function()? onPressMenu,
 }) {
+  final size = MediaQuery.of(context).size;
+  final canPop = Navigator.of(context).canPop();
   return SliverAppBar(
+    leading: SizedBox(),
     floating: true,
     snap: true,
     expandedHeight: 100,
     flexibleSpace: SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          leading ?? const SizedBox.square(dimension: 20.0),
-          Expanded(
-            child: Container(
-              height: 70,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    AppAssets.logoAidcodePng,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: size.width * 0.28,
+              child: leading == null && canPop
+                  ? IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        size: 40,
+                      ),
+                    )
+                  : leading,
+            ),
+            Expanded(
+              child: Container(
+                height: 70,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      AppAssets.logoAidcodePng,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            child: training ??
-                IconButton(
-                  icon: const Icon(Icons.menu, size: 40),
-                  onPressed: onPressMenu,
-                ),
-          ),
-        ],
+            SizedBox(
+              width: size.width * 0.28,
+              child: training ??
+                  IconButton(
+                    icon: const Icon(Icons.menu, size: 40),
+                    onPressed: onPressMenu,
+                  ),
+            ),
+          ],
+        ),
       ),
-    )),
+    ),
   );
 }
