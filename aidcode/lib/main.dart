6905bb92b8f85e2a2ef9ea7ef79080dbe69facff
Eriel Marimon <emarimon@surgeforward.com>
2023-09-30 00:16:57 -0400

    firebase init

--- aidcode/lib/main.dart
+++ aidcode/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:aidcode/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
